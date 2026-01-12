414cb36b9900844e4231d300e5500f67d0439369
AdrianaPikaljov <pikaljovadri@gmail.com>
2026-01-12 12:58:57 +0200

    header veebilehe jaoks on loodud

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <title>Jalgrattaeksam</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header>
+    <h1>Jalgrattaeksam</h1>
+    <nav>
+        <a href="registreerimine.php">Registreerimine</a>
+        <a href="teooriaeksam.php">Teooriaeksam</a>
+        <a href="ringtee.php">Ringtee</a>
+        <a href="slaalom.php">Slaalom</a>
+        <a href="tanav.php">Tänavasõit</a>
+        <a href="lubadeleht.php">Lubadeleht</a>
+    </nav>
+</header>
+
+<main>
